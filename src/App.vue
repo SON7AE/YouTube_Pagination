814a050b45dc9ffcfa5bd87fef860c56f41f9396
SON7AE <bobsot0225@gmail.com>
2023-06-06 10:59:26 +0900

    Vue3+Typescript로 Pagination 기능 개발 - 2. 네비게이션 퍼블리싱

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,11 @@
 <template>
-    <div class="page">TEST</div>
+    <RouterView />
 </template>
 
 <script setup lang="ts"></script>
 
-<style lang="scss" scoped>
-.page {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-    width: 100%;
-    height: 100vh;
-
-    background-color: black; // TEST CODE
-    color: white; // TEST CODE
+<style>
+body {
+    margin: 0px;
 }
 </style>
---
+++ src/components/atoms/layout/BasicLayout.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="basic-layout" :layout-theme="theme">
+        <slot></slot>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { toRefs } from 'vue'
+
+interface Props {
+    theme?: string
+}
+
+const props = defineProps<Props>()
+const { theme } = toRefs(props)
+</script>
+
+<style lang="scss" scoped>
+.basic-layout {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    padding: 20px;
+
+    background-color: $color-white-000;
+    border-radius: 24px;
+
+    &[layout-theme='blue'] {
+        background-color: $color-blue-000;
+    }
+}
+</style>
